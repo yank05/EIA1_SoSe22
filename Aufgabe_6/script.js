6ad5e9f72d01bf081c43d36a2e9dd6ae8cbfe526
yank05 <koenigyannik@web.de>
2022-05-06 14:50:29 +0200

    ADD

---
+++ Aufgabe_6/script.js
@@ -0,0 +1,15 @@
+var BIPDE = 3.570;
+var BIPFR = 2.483;
+var BIPIT = 1.775;
+var BIPBL = 0.560;
+var GesBIPEU = 14.447;
+console.log("Das Bruttoinlandsprodukt Deutschlands lag 2021 bei " + BIPDE + " MRD. Euro.");
+console.log("Das sind " + BIPDE / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.");
+console.log("Das Bruttoinlandsprodukt Frankreichs lag 2021 bei " + BIPFR + " MRD. Euro.");
+console.log("Das sind " + BIPFR / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.");
+console.log("Das Bruttoinlandsprodukt Italiens lag 2021 bei " + BIPIT + " MRD. Euro.");
+console.log("Das sind " + BIPIT / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.");
+console.log("Das Bruttoinlandsprodukt Belgiens lag 2021 bei " + BIPBL + " MRD. Euro.");
+console.log("Das sind " + BIPBL / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.");
+console.log("Das Bruttoinlandsprodukt aller EU-Staaten lag 2021 bei " + GesBIPEU + " MRD. Euro.");
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe_6/script.ts
@@ -0,0 +1,16 @@
+var BIPDE:number = 3.570; 
+var BIPFR:number = 2.483; 
+var BIPIT:number = 1.775;
+var BIPBL:number = 0.560; 
+var GesBIPEU:number = 14.447; 
+
+
+console.log("Das Bruttoinlandsprodukt Deutschlands lag 2021 bei " + BIPDE + " MRD. Euro.")
+console.log("Das sind " + BIPDE / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.")
+console.log("Das Bruttoinlandsprodukt Frankreichs lag 2021 bei " + BIPFR + " MRD. Euro.")
+console.log("Das sind " + BIPFR / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.")
+console.log("Das Bruttoinlandsprodukt Italiens lag 2021 bei " + BIPIT + " MRD. Euro.")
+console.log("Das sind " + BIPIT / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.")
+console.log("Das Bruttoinlandsprodukt Belgiens lag 2021 bei " + BIPBL + " MRD. Euro.")
+console.log("Das sind " + BIPBL / GesBIPEU * 100 + "% Anteil am BIP der gesamten EU.")
+console.log("Das Bruttoinlandsprodukt aller EU-Staaten lag 2021 bei " + GesBIPEU + " MRD. Euro.")
