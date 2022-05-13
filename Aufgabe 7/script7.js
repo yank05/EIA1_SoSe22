861ffbc2470632a3c0a69255bd6c29c2d9e0e469
yank05 <koenigyannik@web.de>
2022-05-13 21:06:59 +0200

    ADD

--- Aufgabe 7/script7.js
+++ Aufgabe 7/script7.js
@@ -3,6 +3,7 @@ var BIPFR = 2.483;
 var BIPIT = 1.775;
 var BIPBL = 0.560;
 var GesBIPEU = 14.447;
+var GesBIPEU2010 = 10.980;
 var BIPDE2010 = 2.564;
 var BIPFR2010 = 1.995;
 var BIPIT2010 = 1.611;
@@ -15,8 +16,9 @@ function clickGermany() {
     document.querySelector("#Text2").innerHTML = "Anteil des BIP Deutschlands am BIP der EU";
     document.querySelector("#Wert3").innerHTML = Math.round(((BIPDE / BIPDE2010 * 100) - 100)) + "%";
     document.querySelector("#Text3").innerHTML = "Wachstum des deutschen BIPs seit 2010";
-    document.querySelector("#Wert4").innerHTML = (BIPFR - BIPFR2010).toFixed(2) + " Billionen Euro";
+    document.querySelector("#Wert4").innerHTML = (BIPDE - BIPDE2010).toFixed(2) + " Billionen Euro";
     document.querySelector("#Text4").innerHTML = "Zunahme des deutschen BIPs seit 2010";
+    document.querySelector(".chartWrapper .chart").setAttribute('style', 'height:25%');
 }
 //Änderung der Werte für Frankreich 
 function clickFrance() {
@@ -26,23 +28,51 @@ function clickFrance() {
     document.querySelector("#Text2").innerHTML = "Anteil des BIP Frankreichs am BIP der EU";
     document.querySelector("#Wert3").innerHTML = Math.round(((BIPFR / BIPFR2010 * 100) - 100)) + "%";
     document.querySelector("#Text3").innerHTML = "Wachstum des französischen BIPs seit 2010";
-    document.querySelector("#Wert4").innerHTML = (BIPDE - BIPDE2010).toFixed(2) + " Billionen Euro";
-    document.querySelector("#Text4").innerHTML = "Zunahme des deutschen BIPs seit 2010";
+    document.querySelector("#Wert4").innerHTML = (BIPFR - BIPFR2010).toFixed(2) + " Billionen Euro";
+    document.querySelector("#Text4").innerHTML = "Zunahme des französischen BIPs seit 2010";
+    document.querySelector(".chartWrapper .chart").setAttribute('style', 'height:17%');
+}
+//Änderung der Werte für Italien
+function clickItaly() {
+    document.querySelector("#Wert1").innerHTML = BIPIT.toFixed(2) + " Billionen Euro";
+    document.querySelector("#Text1").innerHTML = "Bruttoinlandsprodukt Italiens im Jahr 2021";
+    document.querySelector("#Wert2").innerHTML = Math.round(BIPIT / GesBIPEU * 100) + "%";
+    document.querySelector("#Text2").innerHTML = "Anteil des BIP Italiens am BIP der EU";
+    document.querySelector("#Wert3").innerHTML = Math.round(((BIPIT / BIPIT2010 * 100) - 100)) + "%";
+    document.querySelector("#Text3").innerHTML = "Wachstum des italienischen BIPs seit 2010";
+    document.querySelector("#Wert4").innerHTML = (BIPIT - BIPIT2010).toFixed(2) + " Billionen Euro";
+    document.querySelector("#Text4").innerHTML = "Zunahme des italienischen BIPs seit 2010";
+    document.querySelector(".chartWrapper .chart").setAttribute('style', 'height:10%');
+}
+//Änderung der Werte für Belgien
+function clickBelgium() {
+    document.querySelector("#Wert1").innerHTML = BIPBL.toFixed(2) + " Billionen Euro";
+    document.querySelector("#Text1").innerHTML = "Bruttoinlandsprodukt Belgiens im Jahr 2021";
+    document.querySelector("#Wert2").innerHTML = Math.round(BIPBL / GesBIPEU * 100) + "%";
+    document.querySelector("#Text2").innerHTML = "Anteil des BIP Belgiens am BIP der EU";
+    document.querySelector("#Wert3").innerHTML = Math.round(((BIPBL / BIPBL2010 * 100) - 100)) + "%";
+    document.querySelector("#Text3").innerHTML = "Wachstum des belgischen BIPs seit 2010";
+    document.querySelector("#Wert4").innerHTML = (BIPBL - BIPBL2010).toFixed(2) + " Billionen Euro";
+    document.querySelector("#Text4").innerHTML = "Zunahme des belgischen BIPs seit 2010";
+    document.querySelector(".chartWrapper .chart").setAttribute('style', 'height:4%');
+}
+//Klick auf EU-Flagge
+function clickEU() {
+    document.querySelector("#Wert1").innerHTML = GesBIPEU.toFixed(2) + " Billionen Euro";
+    document.querySelector("#Text1").innerHTML = "Bruttoinlandsprodukt aller EU-Staaten im Jahr 2021";
+    document.querySelector("#Wert2").innerHTML = "27";
+    document.querySelector("#Text2").innerHTML = "Staaten bilden das BIP der EU";
+    document.querySelector("#Wert3").innerHTML = Math.round(((GesBIPEU / GesBIPEU2010 * 100) - 100)) + "%";
+    document.querySelector("#Text3").innerHTML = "Wachstum des europäischen BIPs seit 2010";
+    document.querySelector("#Wert4").innerHTML = (GesBIPEU - GesBIPEU2010).toFixed(2) + " Billionen Euro";
+    document.querySelector("#Text4").innerHTML = "Zunahme des europäischen BIPs seit 2010";
+    document.querySelector(".chartWrapper .chart").setAttribute('style', 'height:100%');
 }
 window.addEventListener('load', function () {
     document.querySelector(".germany").addEventListener('click', clickGermany);
     document.querySelector(".france").addEventListener('click', clickFrance);
-}, console.log("Das Bruttoinlandsprodukt Frankreichs lag 2021 bei " + BIPFR + " MRD. Euro."));
-console.log("Dies ist ein Wachstum um " + Math.round(((BIPFR / BIPFR2010 * 100) - 100)) + "% zum Jahr 2010");
-console.log("und " + Math.round(BIPFR / GesBIPEU * 100) + "% Anteil am BIP der gesamten EU.");
-console.log("Das französische BIP stieg in den letzten elf Jahren um " + (BIPFR - BIPFR2010) + " MRD. Euro.");
-console.log("Das Bruttoinlandsprodukt Italiens lag 2021 bei " + BIPIT + " MRD. Euro.");
-console.log("Dies ist ein Wachstum um " + Math.round(((BIPIT / BIPIT2010 * 100) - 100)) + "% zum Jahr 2010");
-console.log("und " + Math.round(BIPIT / GesBIPEU * 100) + "% Anteil am BIP der gesamten EU.");
-console.log("Das itlienische BIP stieg in den letzten elf Jahren um " + (BIPIT - BIPIT2010) + " MRD. Euro.");
-console.log("Das Bruttoinlandsprodukt Belgiens lag 2021 bei " + BIPBL + " MRD. Euro.");
-console.log("Dies ist ein Wachstum um " + Math.round(((BIPBL / BIPBL2010 * 100) - 100)) + "% zum Jahr 2010");
-console.log("und " + Math.round(BIPBL / GesBIPEU) * 100 + "% Anteil am BIP der gesamten EU.");
-console.log("Das belgische BIP stieg in den letzten elf Jahren um " + (BIPBL - BIPBL2010) + " MRD. Euro.");
-console.log("Das Bruttoinlandsprodukt aller EU-Staaten lag 2021 bei " + GesBIPEU + " MRD. Euro.");
+    document.querySelector(".italy").addEventListener('click', clickItaly);
+    document.querySelector(".belgium").addEventListener('click', clickBelgium);
+    document.querySelector(".stars").addEventListener('click', clickEU);
+});
 //# sourceMappingURL=script7.js.map
