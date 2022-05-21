b66468bba6742c271eebfd68625fbf106600abb0
yank05 <koenigyannik@web.de>
2022-05-21 12:54:30 +0200

    ADD

--- Aufgabe_8/script_keyboard.js
+++ Aufgabe_8/script_keyboard.js
@@ -1,2 +1,43 @@
-console.log("Test");
+function playSample(file) {
+    var sound = new Audio("assets/keyboard/" + file);
+    sound.play();
+}
+window.addEventListener('load', function () {
+    document.querySelector("#white1").addEventListener('click', function () {
+        playSample("c.mp3");
+    });
+    document.querySelector("#white2").addEventListener('click', function () {
+        playSample("d.mp3");
+    });
+    document.querySelector("#white3").addEventListener('click', function () {
+        playSample("e.mp3");
+    });
+    document.querySelector("#white4").addEventListener('click', function () {
+        playSample("f.mp3");
+    });
+    document.querySelector("#white5").addEventListener('click', function () {
+        playSample("g.mp3");
+    });
+    document.querySelector("#white6").addEventListener('click', function () {
+        playSample("a.mp3");
+    });
+    document.querySelector("#white7").addEventListener('click', function () {
+        playSample("b.mp3");
+    });
+    document.querySelector("#black1").addEventListener('click', function () {
+        playSample("df.mp3");
+    });
+    document.querySelector("#black2").addEventListener('click', function () {
+        playSample("ef.mp3");
+    });
+    document.querySelector("#black3").addEventListener('click', function () {
+        playSample("gf.mp3");
+    });
+    document.querySelector("#black4").addEventListener('click', function () {
+        playSample("af.mp3");
+    });
+    document.querySelector("#black5").addEventListener('click', function () {
+        playSample("bf.mp3");
+    });
+});
 //# sourceMappingURL=script_keyboard.js.map
