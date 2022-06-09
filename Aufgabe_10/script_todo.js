fd181b80f9a6206fb0c4cb474c3b01020a2f5b2c
yank05 <koenigyannik@web.de>
2022-06-09 15:06:29 +0200

    ADD

--- Aufgabe_10/script_todo.js
+++ Aufgabe_10/script_todo.js
@@ -1,1 +1,20 @@
+//neues Feld beim Enter drücken
+var AddTask = document.getElementById("addTask");
+AddTask.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        let newDiv = document.createElement("div");
+        let newInput = document.createElement("input");
+        let newSpan = document.createElement("span");
+        newDiv.setAttribute("id", "point3");
+        newDiv.setAttribute("class", "point");
+        newDiv.innerHTML = "Den scheiß hier beenden";
+        newInput.setAttribute("type", "radio");
+        newInput.setAttribute("class", "invisible");
+        newSpan.setAttribute("class", "dot");
+        newSpan.setAttribute("id", "point3");
+        document.getElementById("list").appendChild(newDiv);
+        document.getElementById("point3").appendChild(newInput);
+        document.getElementById("point3").appendChild(newSpan);
+    }
+});
 //# sourceMappingURL=script_todo.js.map
