9ff44af2bdfe8b94516822e40598f53794bfdd65
yank05 <koenigyannik@web.de>
2022-06-11 11:50:29 +0200

    ADD

--- Aufgabe_10/script_todo.js
+++ Aufgabe_10/script_todo.js
@@ -1,20 +1,38 @@
-//neues Feld beim Enter drücken
+//Setzen der IDs für neues Element beim Enter drücken
+//Eingabefeld auslesen 
+//Gesamtanzahl ändern
+var pointID = 2;
+var pointText = "point";
+var FieldText = "please write something";
 var AddTask = document.getElementById("addTask");
+AddTask.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        pointID++;
+        FieldText = document.getElementById("addTask").value;
+        console.log(FieldText);
+        document.getElementById("total").innerHTML = pointID + " in total";
+    }
+    if (FieldText == "") {
+        FieldText = "write someting...";
+    }
+});
+//neues Feld erstellen
 AddTask.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         let newDiv = document.createElement("div");
         let newInput = document.createElement("input");
         let newSpan = document.createElement("span");
-        newDiv.setAttribute("id", "point3");
+        newDiv.setAttribute("id", pointText + pointID);
         newDiv.setAttribute("class", "point");
-        newDiv.innerHTML = "Den scheiß hier beenden";
+        newDiv.innerHTML = FieldText;
         newInput.setAttribute("type", "radio");
         newInput.setAttribute("class", "invisible");
         newSpan.setAttribute("class", "dot");
-        newSpan.setAttribute("id", "point3");
+        newSpan.setAttribute("id", pointText + pointID);
         document.getElementById("list").appendChild(newDiv);
-        document.getElementById("point3").appendChild(newInput);
-        document.getElementById("point3").appendChild(newSpan);
+        document.getElementById(pointText + pointID).appendChild(newInput);
+        document.getElementById(pointText + pointID).appendChild(newSpan);
+        console.log(pointText + pointID);
     }
 });
 //# sourceMappingURL=script_todo.js.map
--- Aufgabe_10/script_todo.ts
+++ Aufgabe_10/script_todo.ts
@@ -1,23 +1,38 @@
-//neues Feld beim Enter drücken
+//Setzen der IDs für neues Element beim Enter drücken
+//Eingabefeld auslesen 
+//Gesamtanzahl ändern
+
+var pointID : number = 2; 
+var pointText : string = "point"; 
+var FieldText : string = "please write something"; 
 var AddTask : HTMLElement = document.getElementById("addTask");
 AddTask.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        let newDiv = document.createElement("div");
-        let newInput = document.createElement("input");
-        let newSpan  = document.createElement("span");
-        newDiv.setAttribute("id", "point3"); 
-        newDiv.setAttribute("class", "point"); 
-        newDiv.innerHTML = "Den scheiß hier beenden"; 
-        newInput.setAttribute("type", "radio"); 
-        newInput.setAttribute("class", "invisible"); 
-        newSpan.setAttribute("class", "dot");
-        newSpan.setAttribute("id", "point3"); 
-        document.getElementById("list").appendChild(newDiv);
-        document.getElementById("point3").appendChild(newInput);
-        document.getElementById("point3").appendChild(newSpan); 
-          
-        }
-    })
+        if (event.key === "Enter") {
+            pointID++; 
+            FieldText = document.getElementById("addTask").value; 
+            console.log(FieldText);
+            document.getElementById("total").innerHTML = pointID + " in total";  
+        } 
+        if (FieldText == "") {
+            FieldText = "write someting..."; 
+        }}); 
 
-
-
+//neues Feld erstellen
+AddTask.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            let newDiv = document.createElement("div");
+            let newInput = document.createElement("input");
+            let newSpan  = document.createElement("span");
+            newDiv.setAttribute("id", pointText + pointID); 
+            newDiv.setAttribute("class", "point"); 
+            newDiv.innerHTML = FieldText; 
+            newInput.setAttribute("type", "radio"); 
+            newInput.setAttribute("class", "invisible"); 
+            newSpan.setAttribute("class", "dot");
+            newSpan.setAttribute("id", pointText + pointID); 
+            document.getElementById("list").appendChild(newDiv);
+            document.getElementById(pointText + pointID).appendChild(newInput);
+            document.getElementById(pointText + pointID).appendChild(newSpan);   
+            console.log(pointText + pointID); 
+                }
+            })
